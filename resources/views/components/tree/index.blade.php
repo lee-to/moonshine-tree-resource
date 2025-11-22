8d5930ff73980239607e064291844ad72e255b91
Shevelev Anatolii <tolik.breathless@yandex.ru>
2025-11-22 11:52:19 +0700

    fix(view): при выключении функционала `wrappable` исчезало вообще все дерево

--- resources/views/components/tree/index.blade.php
+++ resources/views/components/tree/index.blade.php
@@ -1,8 +1,6 @@
 @if(!empty($items[0]))
     <div
-        @if($resource->wrapable())
         x-data="{tree_show_all: $persist(true).as('tree_resource_all')}"
-        @endif
     >
         @if($resource->wrapable())
         <a @click.stop="tree_show_all = !tree_show_all">
